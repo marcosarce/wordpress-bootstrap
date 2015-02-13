f0b354e62bdbd4373820328e4b55b25b00774e5d
marcosarce <marcos.ucsd@gmail.com>
2015-02-13 01:16:58 -0800

    removed auto addition of lead class to first paragraph of content

--- functions.php
+++ functions.php
@@ -455,18 +455,6 @@ function wp_bootstrap_add_class_attachment_link( $html ) {
 }
 add_filter( 'wp_get_attachment_link', 'wp_bootstrap_add_class_attachment_link', 10, 1 );
 
-// Add lead class to first paragraph
-function wp_bootstrap_first_paragraph( $content ){
-    global $post;
-
-    // if we're on the homepage, don't add the lead class to the first paragraph of text
-    if( is_page_template( 'page-homepage.php' ) )
-        return $content;
-    else
-        return preg_replace('/<p([^>]+)?>/', '<p$1 class="lead">', $content, 1);
-}
-add_filter( 'the_content', 'wp_bootstrap_first_paragraph' );
-
 // Menu output mods
 class Bootstrap_walker extends Walker_Nav_Menu{
 
